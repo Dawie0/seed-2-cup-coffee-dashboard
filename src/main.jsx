584db7fbf513b96984284f45145c3e6fb749d2bf
Dawid Fourie <dawief4@gmail.com>
2024-02-05 09:33:06 -0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+export const Main = () => {
+  return (
+    <App />
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <Main />
+)
